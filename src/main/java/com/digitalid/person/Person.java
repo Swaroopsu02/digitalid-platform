6e0ddd8bd075f6073edb10bd0a94dbaf1fa5751e
Rohanz-00 <s4144434@student.rmit.edu.au>
2026-02-20 15:09:55 +0400

    Minor bugs fixed in overall code

--- src/main/java/com/digitalid/person/Person.java
+++ src/main/java/com/digitalid/person/Person.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
